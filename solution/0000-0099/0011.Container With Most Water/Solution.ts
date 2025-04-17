a9b7ca0113ad5c1ce2e6dfdd54c49947d5e32db0
cleardevice <cleardevice@gmail.com>
2025-04-17 14:00:35 +0000

    feat: add ts solution to lc problem: No.0011

---
+++ solution/0000-0099/0011.Container With Most Water/Solution.ts
@@ -0,0 +1,15 @@
+function maxArea(height: number[]): number {
+    let l = 0;
+    let r = height.length - 1;
+    let ans = 0;
+    while (l < r) {
+        const t = Math.min(height[l], height[r]) * (r - l);
+        ans = Math.max(ans, t);
+        if (height[l] < height[r]) {
+            l++;
+        } else {
+            r++;
+        }
+    }
+    return ans;
+};
