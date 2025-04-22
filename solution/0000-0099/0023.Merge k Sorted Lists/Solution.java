a3825915231b9cb381deca36eab8a449f440ed6b
cleardevice <cleardevice@gmail.com>
2025-04-22 02:22:25 +0000

    feat: add java solution to lc problem: No.0023

---
+++ solution/0000-0099/0023.Merge k Sorted Lists/Solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        PriorityQueue<ListNode> pq = new PriorityQueue<>((a, b) -> a.val - b.val);
+        for (ListNode head : lists) {
+            if (head != null) {
+                pq.offer(head);
+            }
+        }
+        ListNode dummy = new ListNode();
+        ListNode curr = dummy;
+        while (!pq.isEmpty()) {
+            ListNode node = pq.poll();
+            if (node.next != null) {
+                pq.offer(node.next);
+            }
+            curr.next = node;
+            curr = curr.next;
+        }
+        return dummy.next;
+    }
+}
