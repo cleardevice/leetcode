a3d5183d5f00776e666102b6793e8a32803ebea1
cleardevice <cleardevice@gmail.com>
2025-04-23 03:52:36 +0000

    feat: add ts solution to lc problem: No.0024

---
+++ solution/0000-0099/0024.Swap Nodes in Pairs/Solution2.ts
@@ -0,0 +1,12 @@
+function swapPairs(head: ListNode | null): ListNode | null {
+    const dummy = new ListNode(0, head);
+    let [pre, cur] = [dummy, head];
+    while (cur && cur.next) {
+        const t = cur.next;
+        cur.next = t.next;
+        t.next = cur;
+        pre.next = t;
+        [pre, cur] = [cur, cur.next];
+    }
+    return dummy.next;
+};
