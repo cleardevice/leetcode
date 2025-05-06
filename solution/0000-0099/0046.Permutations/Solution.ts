b41c4615bcbbbe2cf5cf9c60a132160179a40fb5
cleardevice <cleardevice@gmail.com>
2025-05-06 08:12:52 +0000

    feat: add ts solution to lc problem: No.0046

---
+++ solution/0000-0099/0046.Permutations/Solution.ts
@@ -0,0 +1,23 @@
+function permute(nums: number[]): number[][] {
+    const n = nums.length;
+    const ans: number[][] = [];
+    const vis: boolean[] = Array(n).fill(false);
+    const t: number[] = [];
+    const dfs = (i: number) => {
+        if (i >= n) {
+            ans.push(t.slice());
+            return;
+        }
+        for (let j = 0; j < n; j++) {
+            if (vis[j]) {
+                continue;
+            }
+            vis[j] = true;
+            t[i] = nums[j];
+            dfs(i + 1);
+            vis[j] = false;
+        }
+    }
+    dfs(0);
+    return ans;
+};
