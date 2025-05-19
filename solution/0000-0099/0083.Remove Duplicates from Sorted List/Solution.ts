8e9d7d8a5d13d2d40c4bec71a6ee5c8d5b71b584
cleardevice <cleardevice@gmail.com>
2025-05-19 07:54:42 +0000

    feat: add ts solution to lc problem: No.0083

---
+++ solution/0000-0099/0083.Remove Duplicates from Sorted List/Solution.ts
@@ -0,0 +1,11 @@
+function deleteDuplicates(head: ListNode | null): ListNode | null {
+    let cur = head;
+    while (cur && cur.next) {
+        if (cur.val === cur.next.val) {
+            cur.next = cur.next.next;
+        } else {
+            cur = cur.next;
+        }
+    }
+    return head;
+};
