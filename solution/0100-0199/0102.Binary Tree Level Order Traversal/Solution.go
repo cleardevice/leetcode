91146f32dbaa49efb333d9689477bbf2157954f5
cleardevice <cleardevice@gmail.com>
2025-06-03 10:22:03 +0000

    feat: add go solution to lc problem: No.0102.Binary Tree Level Order Traversal

---
+++ solution/0100-0199/0102.Binary Tree Level Order Traversal/Solution.go
@@ -0,0 +1,22 @@
+func levelOrder(root *TreeNode) (ans [][]int) {
+    if root == nil {
+        return
+    }
+    q := []*TreeNode{root}
+    for len(q) > 0 {
+        t := []int{}
+        for n := len(q); n > 0; n-- {
+            node := q[0]
+            q = q[1:]
+            t = append(t, node.Val)
+            if node.Left != nil {
+                q = append(q, node.Left)
+            }
+            if node.Right != nil {
+                q = append(q, node.Right)
+            }
+        }
+        ans = append(ans, t)
+    }
+    return
+}
