6eed9564a28d046ffe979d92a37b9db732cb7bdc
cleardevice <cleardevice@gmail.com>
2025-06-07 10:16:35 +0000

    feat: add java solution 2 to lc problem: No.0111.Minimum Depth of Binary Tree

---
+++ solution/0100-0199/0111.Minimum Depth of Binary Tree/Solution2.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        Deque<TreeNode> q = new ArrayDeque<>();
+        q.offer(root);
+        int ans = 0;
+        while (true) {
+            ++ans;
+            for (int n = q.size(); n > 0; n--) {
+                TreeNode node = q.poll();
+                if (node.left == null && node.right == null) {
+                    return ans;
+                }
+                if (node.left != null) {
+                    q.offer(node.left);
+                }
+                if (node.right != null) {
+                    q.offer(node.right);
+                }
+            }
+        }
+    }
+}
