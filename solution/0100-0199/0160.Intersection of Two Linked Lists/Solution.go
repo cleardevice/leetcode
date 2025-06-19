8bc28875fee02a0c030997e8a6035fac4a9172bd
cleardevice <cleardevice@gmail.com>
2025-06-19 14:38:24 +0000

    feat: add go solution to lc problem: No.0160.Intersection of Two Linked Lists

---
+++ solution/0100-0199/0160.Intersection of Two Linked Lists/Solution.go
@@ -0,0 +1,16 @@
+func getIntersectionNode(headA, headB *ListNode) *ListNode {
+	a, b := headA, headB
+	for a != b {
+		if a == nil {
+			a = headB
+		} else {
+			a = a.Next
+		}
+		if b == nil {
+			b = headA
+		} else {
+			b = b.Next
+		}
+	}
+	return a
+}
