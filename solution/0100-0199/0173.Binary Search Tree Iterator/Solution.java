a239824cc51117439445748698065b8a4f529d73
cleardevice <cleardevice@gmail.com>
2025-06-25 01:01:08 +0000

    feat: add java solution to lc problem: No.0173.Binary Search Tree Iterator

---
+++ solution/0100-0199/0173.Binary Search Tree Iterator/Solution.java
@@ -0,0 +1,24 @@
+class BSTIterator {
+    private int cur = 0;
+    private List<Integer> vals = new ArrayList<>();
+
+    public BSTIterator(TreeNode root) {
+        inorder(root);
+    }
+
+    public int next() {
+        return vals.get(cur++);
+    }
+
+    public boolean hasNext() {
+        return cur < vals.size();
+    }
+
+    private void inorder(TreeNode root) {
+        if (root != null) {
+            inorder(root.left);
+            vals.add(root.val);
+            inorder(root.right);
+        }
+    }
+}
