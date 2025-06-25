c55257104c256251716ec9eeaef4e6f6c7ffdff3
cleardevice <cleardevice@gmail.com>
2025-06-25 18:10:44 +0000

    feat: add java 2 solution to lc problem: No.0173.Binary Search Tree Iterator

---
+++ solution/0100-0199/0173.Binary Search Tree Iterator/Solution2.java
@@ -0,0 +1,21 @@
+class BSTIterator {
+    private Deque<TreeNode> stack = new LinkedList<>();
+
+    public BSTIterator(TreeNode root) {
+        for (; root != null; root = root.left) {
+            stack.offerLast(root);
+        }
+    }
+
+    public int next() {
+        TreeNode cur = stack.pollLast();
+        for (TreeNode node = cur.right; node != null; node = node.left) {
+            stack.offerLast(node);
+        }
+        return cur.val;
+    }
+
+    public boolean hasNext() {
+        return !stack.isEmpty();
+    }
+}
