a7d75c334b2518c28d8f9c01e0f4fee6f162a889
cleardevice <cleardevice@gmail.com>
2025-07-03 00:59:31 +0000

    feat: add java 2 solution to lc problem: No.0199.Binary Tree Right Side View

---
+++ solution/0100-0199/0199.Binary Tree Right Side View/Solution2.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        if (root == null) {
+            return ans;
+        }
+        Deque<TreeNode> q = new ArrayDeque<>();
+        q.offer(root);
+        while (!q.isEmpty()) {
+            ans.add(q.peekFirst().val);
+            for (int k = q.size(); k > 0; --k) {
+                TreeNode node = q.poll();
+                if (node.right != null) {
+                    q.offer(node.right);
+                }
+                if (node.left != null) {
+                    q.offer(node.left);
+                }
+            }
+        }
+        return ans;
+    }
+}
